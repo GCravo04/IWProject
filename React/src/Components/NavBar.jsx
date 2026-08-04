e271e65c4d43341505c0ca395d2a0c792685db98
GCravo04 <gabicravo04@gmail.com>
2026-08-04 17:07:25 +0100

    alteração do estilo da navbar

--- React/src/Components/NavBar.jsx
+++ React/src/Components/NavBar.jsx
@@ -1,37 +1,81 @@
 import { Link } from "react-router-dom";
+import "../Css/Navbar.css";
 
-function Navbar() {
+function NavBar() {
+
+    const token = localStorage.getItem("token");
 
     return (
 
         <nav className="navbar">
 
-            <h2>Social</h2>
+            <Link to="/" className="logo">
+                Social
+            </Link>
 
-            <div>
+            <div className="nav-links">
 
-                <Link to="/">
-                    Home
-                </Link>
+                <Link to="/">Home</Link>
 
-                <Link to="/profile">
-                    Perfil
-                </Link>
+                {token && (
+                    <Link to="/profile">
+                        Perfil
+                    </Link>
+                )}
 
-                <Link to="/login">
-                    Login
-                </Link>
+            </div>
 
-                <Link to="/register">
-                    Register
-                </Link>
+            <div className="nav-right">
+
+                {!token ? (
+                    <>
+                        <Link
+                            className="login-btn"
+                            to="/login"
+                        >
+                            Login
+                        </Link>
+
+                        <Link
+                            className="register-btn"
+                            to="/register"
+                        >
+                            Registar
+                        </Link>
+                    </>
+                ) : (
+                    <>
+                        <Link
+                            to="/profile"
+                            className="profile-link"
+                        >
+                            <img
+                                src="https://placehold.co/80"
+                                alt=""
+                            />
+
+                            <span>Perfil</span>
+                        </Link>
+
+                        <button
+                            className="logout-btn"
+                            onClick={() => {
+
+                                localStorage.removeItem("token");
+                                window.location.href = "/login";
+
+                            }}
+                        >
+                            Logout
+                        </button>
+                    </>
+                )}
 
             </div>
 
         </nav>
 
     );
-
 }
 
-export default Navbar;
+export default NavBar;
